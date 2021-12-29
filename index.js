ac313aa3fed2e50ef2250b9f755da189dc420273
Nirmal2349 <89510211+Nirmal2349@users.noreply.github.com>
2021-12-29 10:20:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+setTimeout(function () {
+  document.getElementById("countdown").innerHTML = "10";
+  setTimeout(function () {
+    document.getElementById("countdown").innerHTML = "9";
+    setTimeout(function () {
+      document.getElementById("countdown").innerHTML = "8";
+      setTimeout(function () {
+        document.getElementById("countdown").innerHTML = "7";
+        setTimeout(function () {
+          document.getElementById("countdown").innerHTML = "6";
+          setTimeout(function () {
+            document.getElementById("countdown").innerHTML = "5";
+            setTimeout(function () {
+              document.getElementById("countdown").innerHTML = "4";
+              setTimeout(function () {
+                document.getElementById("countdown").innerHTML = "3";
+                setTimeout(function () {
+                  document.getElementById("countdown").innerHTML = "2";
+                  setTimeout(function () {
+                    document.getElementById("countdown").innerHTML = "1";
+                    setTimeout(function () {
+                      document.getElementById("countdown").innerHTML =
+                        "Happy Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
